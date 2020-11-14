330820ca414eba6a9b371cfb224387bd1b4576ef
MathiasJoly <62999527+MathiasJoly@users.noreply.github.com>
2020-11-14 18:52:57 +0100

    Update HelloWorld.cpp
    En s'appuyant sur le fichier SkeletonMPIProgramWithFilesOutput.cpp

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -1,22 +1,30 @@
-# include <iostream>
 # include <cstdlib>
-# include <mpi.h>
 # include <sstream>
+# include <string>
 # include <fstream>
+# include <iostream>
+# include <iomanip>
+# include <mpi.h>
 
 int main( int nargs, char* argv[] )
 {
-    MPI_Init(&nargs, &argv);
-    int rang_tache, nombre_taches;
+	MPI_Init( &nargs, &argv );
+	MPI_Comm globComm;
+	MPI_Comm_dup(MPI_COMM_WORLD, &globComm);
+	int nombre_taches;
+	MPI_Comm_size(globComm, &nombre_taches);
+	int rang_tache;
+	MPI_Comm_rank(globComm, &rang_tache);
 
-    MPI_Comm_rank(MPI_COMM_WORLD,
-                  &rang_tache);
-    MPI_Comm_size(MPI_COMM_WORLD, 
-                  &nombre_taches);
-    std::cout << "Bonjour, je suis la tâche n° " 
-              << rand_tache << " sur "
-              << nombre_taches << " tâche." 
-              << std::endl;
-    MPI_Finalize();
-    return EXIT_SUCCESS;
+	// Création d'un fichier pour ma propre sortie en écriture :
+	std::stringstream fileName;
+	fileName << "Output" << std::setfill('0') << std::setw(5) << rang_tache << ".txt";
+	std::ofstream output( fileName.str().c_str() );
+
+	output << "Bonjour, je suis la tâche n° " << rang_tache << " sur " << nombre_taches << " tâches." << std::endl;
+
+	output.close();
+
+	MPI_Finalize();
+	return EXIT_SUCCESS;
 }
