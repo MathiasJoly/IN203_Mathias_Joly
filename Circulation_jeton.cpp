19011612bb41b14c03f75ee65f4e0167cd16b946
MathiasJoly <62999527+MathiasJoly@users.noreply.github.com>
2020-11-15 12:33:21 +0100

    Update Circulation_jeton.cpp
    elimination des commentaires

--- Circulation_jeton.cpp
+++ Circulation_jeton.cpp
@@ -9,12 +9,9 @@ int main(int argc , char *argv [])
 {
 	int nbp , rank , next , prev , tag=1 , jeton;
 	MPI_Status stats;
-	// required variable for non - blocking calls
-	// required variable for Waitall routine
 	MPI_Init (& argc ,& argv);
 	MPI_Comm_size ( MPI_COMM_WORLD , &nbp );
 	MPI_Comm_rank ( MPI_COMM_WORLD , &rank);
-	// determine left and right neighbors
 	prev = rank -1;
 	next = rank +1;
 	if (rank == ( nbp - 1)) next = 0;
