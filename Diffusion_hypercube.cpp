8c2ad2b2360d6715c293c856dc94470c79b870a9
MathiasJoly <62999527+MathiasJoly@users.noreply.github.com>
2020-11-15 14:00:42 +0100

    Update Diffusion_hypercube.cpp
    Dimension 2

--- Diffusion_hypercube.cpp
+++ Diffusion_hypercube.cpp
@@ -11,17 +11,27 @@ int main(int argc , char *argv [])
 	MPI_Status stats;
 	
 	MPI_Init (& argc ,& argv);
-	MPI_Comm_size ( MPI_COMM_WORLD , &nbp ); //nbp=2
+	MPI_Comm_size ( MPI_COMM_WORLD , &nbp ); //nbp=4
 	MPI_Comm_rank ( MPI_COMM_WORLD , &rank);
 	
 	if (rank == 0)
 	{
 		jeton = 6;
-		MPI_Send (&jeton , 1, MPI_INT , 1 , tag , MPI_COMM_WORLD );
+		MPI_Send (&jeton, 1, MPI_INT, 1, 1, MPI_COMM_WORLD);
+		MPI_Send (&jeton, 1, MPI_INT, 3, 3, MPI_COMM_WORLD);
 	}
-	else
+	else if (rank == 1)
 	{
-		MPI_Recv (&jeton , 1, MPI_INT , 0 , tag , MPI_COMM_WORLD , &stats);
+		MPI_Recv (&jeton, 1, MPI_INT, 0, 1 , MPI_COMM_WORLD, &stats);
+		MPI_Send (&jeton, 1, MPI_INT, 2, 2 , MPI_COMM_WORLD);
+	}
+	else if (rank == 2)
+	{
+		MPI_Recv (&jeton, 1, MPI_INT, 1, 2 , MPI_COMM_WORLD, &stats);
+	}
+	else if (rank == 3)
+	{
+		MPI_Recv (&jeton, 1, MPI_INT, 0, 3 , MPI_COMM_WORLD, &stats);
 	};
 
 	std::cout << "tache n° " << rank << ", jeton = " << jeton << "." << std::endl;
